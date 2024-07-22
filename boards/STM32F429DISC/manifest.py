575ef8fde1bbeea755112eec1e3b4161b1b1825b
jsheprd <166157214+jsheprd@users.noreply.github.com>
2024-07-22 14:49:29 -0700

    Add files via upload

--- boards/STM32F429DISC/manifest.py
+++ boards/STM32F429DISC/manifest.py
@@ -2,5 +2,12 @@ include("$(PORT_DIR)/boards/manifest.py")
 
 module("touch811.py")
 module("lcd9341.py")
-module("xglcd_font.py")
+module("xfglcd_font.py")
+module("ArcadePix9x11_Froze.py")
+module("Bally7x9_Froze.py")
+module("Broadway17x15_Froze.py")
+module("EspressoDolce18x24_Froze.py")
+module("IBMPlexMono12x24_Froze.py")
+module("Robotron13x21_Froze.py")
+module("Unispace12x24_Froze.py")
 
